e2dbc3c6814544c8a440d04f1d23a6731a099948
jandrogarrido791 <9212355939aledelacruz@gmail.com>
2026-07-01 14:03:11 -0600

    Create Creación del archivo Principal.java

---
+++ Creación del archivo Principal.java
@@ -0,0 +1,18 @@
+// Clase principal
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+        // Creación del objeto estudiante
+        Estudiante estudiante1 = new Estudiante(
+            "Jesús Alejandro de la Cruz Garrido",
+            "12345",
+            "Ingeniería en Desarrollo de Software"
+        );
+
+        // Uso del método
+        estudiante1.mostrarDatos();
+
+    }
+}
